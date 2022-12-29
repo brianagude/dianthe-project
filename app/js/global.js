a5f78acd4434771998670d6155d86a6ceba13a9b
briana gude <52926266+brianagude@users.noreply.github.com>
2022-12-29 13:39:22 -0500

    Updates 2 (#2)
    * fixing bottom form
    
    * reorg stylesheets and test bottom bar
    
    * finally fixedddd
    
    * bugs fixed woo
    
    * update margins

--- app/js/global.js
+++ app/js/global.js
@@ -1,8 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // const iframe = document.getElementById('toast-3b9f5dd9-6a47-4827-a65e-0e0e8e97fb18')
-  // const funraise = document.getElementById('funraise-btn')
+  // DETECT WHICH BROWSER IS BEING USED ================================
+  function fnBrowserDetect(){
+    let userAgent = navigator.userAgent;
+    let browserName;
+    
+    if(userAgent.match(/chrome|chromium|crios/i)){
+      browserName = "chrome";
+    }else if(userAgent.match(/firefox|fxios/i)){
+      browserName = "firefox";
+    }  else if(userAgent.match(/safari/i)){
+      browserName = "safari";
+    }else if(userAgent.match(/opr\//i)){
+      browserName = "opera";
+    } else if(userAgent.match(/edg/i)){
+      browserName = "edge";
+    }else{
+      browserName="No browser detection";
+    }
+    
+    document.querySelector("body").classList.add(browserName);         
+  }
+
+  fnBrowserDetect()
 
 
+  // HEADER TOGGLE ==============================================================
   const checkbox = document.getElementById('checkbox')
   const mainContent = document.getElementById("mainContent");
 
@@ -10,12 +32,13 @@ document.addEventListener("DOMContentLoaded", function () {
     mainContent.classList.toggle("slide");
   })
 
+  // CAROUSEL TEXT ==============================================================
   const carouselText = [
     {text: "You have reached the Dianthe Project.", color: "#be4310"},
     {text: "( pronounced dye / ANN / thee )", color: "#82b8d4"},
     {text: "We are a non-profit organization that inspires, supports, and trains single black mothers living on the margins to pursue professional and entrepreneurial careers in the tech industry.", color: "#120f02"},
-    {text: "This summer, we will be opening our doors to moms in Atlanta, GA.", color: "#120f02"},
-    {text: "Please join our mailing list or follow us on Instagram to learn more.", color: "#e3b722"}
+    {text: "This summer, we will be opening our doors to moms in Atlanta, GA.", color: "#e3b722"},
+    {text: "Please join our mailing list or follow us on Instagram to learn more.", color: "#120f02"}
   ]
 
   async function typeSentence(sentence, eleRef, delay = 100) {
